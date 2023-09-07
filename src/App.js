9b1d6f0d39b6c2c320bce89e99b1a1d14bb0186a
AmnaZa <113046745+AmnaZa@users.noreply.github.com>
2023-09-07 00:18:36 -0400

    Auth page added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import AuthPage from './pages/AuthPage';
+
 
 function App() {
+  // Define the user state using useState
+  const [user, setUser] = useState({});
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App">
+      {/* Render the AuthPage component */}
+      <AuthPage />
+    </main>
   );
 }
 
