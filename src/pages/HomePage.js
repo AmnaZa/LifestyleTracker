fb673ab73b8b2b94dab7d4383f7714a83ce75f57
AmnaZa <113046745+AmnaZa@users.noreply.github.com>
2023-09-16 00:11:08 -0400

    audio added

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import NavBar from '../components/NavBar';
-import '../App.css';
+
 import './home.css'; // Import your custom styles
+import CalmMusic from './Calm.mp3';
 
 function HomePage({ user, setUser }) {
+  const [volume, setVolume] = useState(0.5); // Initial volume is set to 0.5 (50%)
+
+  const handleVolumeChange = (e) => {
+    setVolume(parseFloat(e.target.value));
+    const audioElement = document.getElementById('background-music');
+    audioElement.volume = parseFloat(e.target.value);
+  };
+
+  useEffect(() => {
+    // Start audio playback when the component mounts
+    const audioElement = document.getElementById('background-music');
+    audioElement.play();
+  }, []);
+
   return (
     <div>
       <NavBar /> {/* Render the navigation bar on the home page */}
@@ -12,50 +27,54 @@ function HomePage({ user, setUser }) {
           <title>FIT-MIND</title>
           <link href="https://fonts.googleapis.com/css?family=Courgette|Open+Sans&display=swap" rel="stylesheet" />
 
+          {/* Use the standard HTML5 <audio> element with the same class and ID */}
+          <div className="audio-container">
+            <audio id="background-music" controls autoPlay volume={volume}>
+              <source src={CalmMusic} type="audio/mpeg" />
+              Your browser does not support the audio element.
+            </audio>
+
+            <div className="audio-controls">
+              <input
+                type="range"
+                id="background-music"
+                className="volume-control"
+                min="0"
+                max="1"
+                step="0.01"
+                value={volume}
+                onChange={handleVolumeChange}
+              />
+            </div>
+          </div>
+
           <section>
             <h2>Hello, {user ? user.name : 'Guest'}!</h2>
             <div className="glowing">
-
               <span style={{ '--i': '1' }}></span>
-
               <span style={{ '--i': '2' }}></span>
-
               <span style={{ '--i': '3' }}></span>
-
             </div>
 
             <div className="glowing">
-
               <span style={{ '--i': '1' }}></span>
-
               <span style={{ '--i': '2' }}></span>
-
               <span style={{ '--i': '3' }}></span>
-
             </div>
 
             <div className="glowing">
-
               <span style={{ '--i': '1' }}></span>
-
               <span style={{ '--i': '2' }}></span>
-
               <span style={{ '--i': '3' }}></span>
-
             </div>
 
             <div className="glowing">
-
               <span style={{ '--i': '1' }}></span>
-
               <span style={{ '--i': '2' }}></span>
-
               <span style={{ '--i': '3' }}></span>
-
             </div>
 
           </section>
-
         </div>
       </div>
     </div>
